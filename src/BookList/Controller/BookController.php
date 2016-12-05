780159892ca46e1d1948826d763f6c314fae5e6a
oshaman <reg_forall@bigmir.net>
2016-12-05 12:34:33 +0200

    add view

--- src/BookList/Controller/BookController.php
+++ src/BookList/Controller/BookController.php
@@ -2,27 +2,66 @@
 namespace BookList\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use BookList\Form\BookForm;
 
 class BookController extends AbstractActionController
 {
     public function indexAction()
     {
-        //echo 'TEST';
+        return new ViewModel(array(
+		    'books' => [
+			    
+			]
+		));
     }
 	
 	public function addAction()
     {
-        //echo 'TEST';
+        //отрисовка форм
+		$form = new BookForm();
+		$form->get('submit')->setValue('Add');
+		//и отработка запросов(если такие есть)
+		$request = $this->getRequest();
+		if($request->isPost()) {
+			
+		}
+		return array('form' => $form);
     }
 	
 	public function editAction()
     {
-        //echo 'TEST';
+       //отрисовка форм
+		$form = new BookForm();
+		//привязыйзваем к БД
+		//$form->bind($book);
+		$form->get('submit')->setAttribute('value', 'Edit');
+		//и отработка запросов(если такие есть)
+		$request = $this->getRequest();
+		if($request->isPost()) {
+			
+		}
+		return array(
+		    'id' => $id,
+            'form' => $form
+		);
     }
 	
 	public function deleteAction()
     {
-        //echo 'TEST';
+        $id = (int) $this->params()->fromRoute('id', 0);
+		if (!$id) {
+			return $this->redirect()->toRoute('book');
+		}
+		$request = $this->getRequest();
+		if($request->isPost()) {
+			
+		}
+		return array(
+		    'id' => $id,
+            //'book' => 
+		);
+		
     }
 }
  
---
+++ src/BookList/Form/BookForm.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BookList\Form;
+
+use Zend\Form\Form;
+
+class BookForm extends Form
+{
+	public function __construct($name = null)
+	{
+		//we want to ignore the name passed
+		parent::__construct('book');
+		
+		$this->add(array(
+		    'name' => 'id',
+		    'type' => 'Hidden',
+		));
+	    
+		$this->add(array(
+		    'name' => 'title',
+		    'type' => 'Text',
+		    'options' => array(
+			    'label' => 'Title',
+			),
+		));
+		
+		$this->add(array(
+		    'name' => 'author',
+		    'type' => 'Text',
+		    'options' => array(
+			    'label' => 'author',
+			),
+		));
+		
+		$this->add(array(
+		    'name' => 'submit',
+		    'type' => 'Submit',
+		    'attributes' => array(
+			    'value' => 'Go',
+			    'id' => 'submitbutton',
+			),
+		));
+	}
+}
